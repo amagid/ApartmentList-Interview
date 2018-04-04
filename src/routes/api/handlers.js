d079950be813e5311028291ea581d5fefc408db2
Aaron Magid <ahm64@case.edu>
2018-04-04 19:09:42 -0400

    feat(FamilyFriday): Copied over FamilyFriday code for use in the application.

---
+++ src/routes/api/handlers.js
@@ -0,0 +1,49 @@
+module.exports = {
+    getGroupings
+};
+
+function getGroupings(names, groupSize) {
+    return Promise.resolve(generateGroups(JSON.parse(names), parseInt(groupSize)));
+}
+
+
+
+
+
+
+
+function shuffle(names) {
+    let index, temp;
+    for (let i = 0; i < names.length; i++) {
+        index = Math.floor(Math.random() * names.length);
+        temp = names[index];
+        names[index] = names[i];
+        names[i] = temp;
+    }
+    return names;
+}
+
+function generateGroups(names, groupSize) {
+    let groups = [];
+    let i = 0;
+    //Iterate in steps of 4 and add groups to output list
+    while (i < names.length - (groupSize - 1)) {
+        //General case, can take a group of 4
+        if (names.length - i >= groupSize) {
+            groups.push(names.slice(i, i + groupSize));
+            i += groupSize;
+        }
+    }
+    let currentGroup = groups.length - 1;
+    while (i < names.length) {
+        groups[currentGroup].push(names[i]);
+        i++;
+        currentGroup--;
+    }
+    return groups;
+}
+
+function createGroups(names, groupSize) {
+    shuffle(names);
+    return generateGroups(names, groupSize);
+}
